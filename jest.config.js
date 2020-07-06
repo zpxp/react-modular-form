0e75f0b9ef93afafbd3243422c702720f6eabdbd
brendo <admin@brendanmorrel.com>
2020-07-06 15:09:35 +0800

    bootstrap

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,17 @@
 module.exports = {
-	collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
 	roots: ["<rootDir>/src"],
-	resolver: "jest-pnp-resolver",
+	testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
+	transform: {},
 	setupFilesAfterEnv: ["<rootDir>/config/jest/setupTests.js"],
-	testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(tsx?|jsx?)$",
-	testPathIgnorePatterns: ["\\.notest\\."],
+	testPathIgnorePatterns: [".notest."],
 	testEnvironment: "jsdom",
 	testURL: "http://localhost",
 	transform: {
 		"^.+\\.(js|jsx)$": "<rootDir>/node_modules/babel-jest",
-		"^.+\\.(ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+		"^.+\\.(ts|tsx)$": "ts-jest",
 		"^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
 		"^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "<rootDir>/config/jest/fileTransform.js"
 	},
-	transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$", "^.+\\.module\\.(css|sass|scss)$"],
-	moduleNameMapper: {
-		"^react-native$": "react-native-web",
-		"^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy"
-	},
 	moduleDirectories: ["node_modules", "src"],
-	moduleFileExtensions: ["web.js", "js", "json", "web.jsx", "jsx", "tsx", "ts", "node"]
+	transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$", "^.+\\.module\\.(css|sass|scss)$"]
 };
